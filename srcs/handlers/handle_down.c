5d05344ed2df1321095651a9bad07735b1e0ead5
Pierre-quentin Deguingand <pierrequentindeguingand@gmail.com>
2018-10-13 17:14:23 -0700

    Finally got up and down to work properly with columns

--- srcs/handlers/handle_down.c
+++ srcs/handlers/handle_down.c
@@ -6,7 +6,7 @@
 /*   By: pdeguing <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/11 19:03:17 by pdeguing          #+#    #+#             */
-/*   Updated: 2018/10/11 19:13:10 by pdeguing         ###   ########.fr       */
+/*   Updated: 2018/10/13 17:13:36 by pdeguing         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,22 @@
 void	handle_down(t_select *s)
 {
 	int		i;
+	int		padding;
 
 	i = 0;
+	padding = 0;
 	while (i < s->p_col)
 	{
 		*s->cursor = (*s->cursor)->next;
+		if (*s->cursor == *s->dlist)
+		{
+			if (padding >= 0)
+				i -= s->lst_size % s->p_col;
+			if (padding < s->lst_size % s->p_col)
+				i += s->p_col;
+		}
+		else
+			padding++;
 		i++;
 	}
 }
--- srcs/handlers/handle_up.c
+++ srcs/handlers/handle_up.c
@@ -6,7 +6,7 @@
 /*   By: pdeguing <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/11 18:47:01 by pdeguing          #+#    #+#             */
-/*   Updated: 2018/10/11 19:13:21 by pdeguing         ###   ########.fr       */
+/*   Updated: 2018/10/13 17:13:38 by pdeguing         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,20 @@
 void	handle_up(t_select *s)
 {
 	int		i;
+	int		padding;
 
 	i = 0;
+	padding = 0;
 	while (i < s->p_col)
 	{
+		if (*s->cursor == *s->dlist)
+		{
+			i -= s->lst_size % s->p_col;
+			if (padding < s->lst_size % s->p_col)
+				i += s->p_col;
+		}
+		else
+			padding++;
 		*s->cursor = (*s->cursor)->prev;
 		i++;
 	}
